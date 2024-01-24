e2e78390419f867c94c9f15f1610c4be3d97b9c9
lorne-rtm <157533465+lorne-rtm@users.noreply.github.com>
2024-01-24 15:14:46 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,13 +12,28 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx_design',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'myst_parser',
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinxext.rediraffe",
+    "sphinx_design",
+    "sphinx_copybutton",
+    "autoapi.extension",
+    # custom extentions
+    "_extension.gallery_directive",
+    "_extension.component_directive",
+    # For extension examples and demos
+    "myst_parser",
+    "ablog",
+    "jupyter_sphinx",
+    "sphinxcontrib.youtube",
+    "nbsphinx",
+    "numpydoc",
+    "sphinx_togglebutton",
+    "jupyterlite_sphinx",
+    "sphinx_favicon",
 ]
 
 intersphinx_mapping = {
